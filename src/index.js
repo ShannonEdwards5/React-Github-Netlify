887c6e99b266b3f6bdd77f0305dde25b3c09e8de
ShannonEdwards5 <162493542+ShannonEdwards5@users.noreply.github.com>
2024-06-15 18:02:58 +0200

    Updated links & css

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Cape Town");
+searchCity("Johannesburg");
 
 displayForecast();
 
